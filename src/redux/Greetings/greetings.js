a04925751dcde0be2a3d87eb42458e9e67e16fd4
Zeeshan Haider <zhadier39@gmail.com>
2022-05-31 22:20:32 +0500

    Configure the store

---
+++ src/redux/Greetings/greetings.js
@@ -0,0 +1,36 @@
+// constants
+const GET_GREETINGS = 'GREETINGS/GET_GREETINGS';
+
+// initial state
+const initialState = 'Hello World';
+
+// action creators
+export const getGreetings = (payload) => ({
+  type: GET_GREETINGS,
+  payload,
+});
+
+// thunk action functions
+export const getGreetingsFromAPI = () => async (dispatch) => {
+  await fetch('/v1/greetings')
+    .then((response) => response.json())
+    .then((json) => {
+      dispatch(getGreetings(json.greeting));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_GREETINGS: {
+      return action.payload;
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
